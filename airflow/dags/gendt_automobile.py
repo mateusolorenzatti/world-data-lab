46983d6bd79cfd7d1dafc1034076104e4dd4d880
Mateus Orlandin Lorenzatti <mateusolorenzatti@gmail.com>
2026-05-26 00:07:29 -0300

    WDL-S2 | Dag now visible in airflow; Still working on finishing dag run with sucess

---
+++ airflow/dags/gendt_automobile.py
@@ -0,0 +1,49 @@
+from airflow import DAG
+from airflow.providers.standard.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    'GENDT_Automobile_Pipeline',
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+    tags=["gendt", "automobile", "pipeline"],
+    description="Pipeline completo: Bronze → Silver → Gold"
+) as dag:
+    
+    bronze_task = BashOperator(
+        task_id='bronze_ingestion',
+        bash_command="""
+        jupyter nbconvert --to notebook --execute \
+        --output /opt/airflow/logs/bronze_automobile_{{ds}}.ipynb \
+        /opt/airflow/notebooks/src/gendt/1_brz/brz_automobile.ipynb
+        """
+    )
+    
+    silver_task = BashOperator(
+        task_id='silver_transformation',
+        bash_command="""
+        jupyter nbconvert --to notebook --execute \
+        --output /opt/airflow/logs/silver_automobile_{{ds}}.ipynb \
+        /opt/airflow/notebooks/src/gendt/2_slv/slv_automobile.ipynb
+        """
+    )
+    
+    gold_task_brand = BashOperator(
+        task_id='gold_aggregation_brand',
+        bash_command="""
+        jupyter nbconvert --to notebook --execute \
+        --output /opt/airflow/logs/gold_automobile_{{ds}}.ipynb \
+        /opt/airflow/notebooks/src/gendt/3_gld/gld_automobile_by_brand.ipynb
+        """
+    )
+    
+    gold_task_country = BashOperator(
+        task_id='gold_aggregation_country',
+        bash_command="""
+        jupyter nbconvert --to notebook --execute \
+        --output /opt/airflow/logs/gold_automobile_{{ds}}.ipynb \
+        /opt/airflow/notebooks/src/gendt/3_gld/gld_automobile_by_country.ipynb
+        """
+    )
+    
+    bronze_task >> silver_task >> [gold_task_brand, gold_task_country]
